bec8fe261ae8c76414210f3bd107f58aed2b8383
JorgeBueu <freirebueu@gmail.com>
2026-02-23 19:50:18 +0100

    Actividade 4.13 crear TableController y mostrar una tabla

--- src/Controller/TableController.php
+++ src/Controller/TableController.php
@@ -8,8 +8,8 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class TableController extends AbstractController
 {
-    #[Route('/table/{filas}/{cols?4<[1-9]\d*>}', name: 'app_table', requirements: ["filas" => "[1-9]\d*"])]
-    public function index(int $filas = 4, int $cols): Response
+    #[Route('/table/{filas}/{cols<[1-9]\d*>}', name: 'app_table', requirements: ["filas" => "[1-9]\d*"])]
+    public function index(int $filas = 4, int $cols = 4): Response
     {
         $array = [];
         for ($i=0; $i < $filas ; $i++) {
